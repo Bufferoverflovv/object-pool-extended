fdc46c001a0e327ade3813d7631f70e6d6c45a76
Jordan <49358147+Bufferoverflovv@users.noreply.github.com>
2023-08-15 14:41:05 +1000

    Create IPoolObject.cs

---
+++ IPoolObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Buffer.ObjectPooling
+{
+    public interface IPoolObject
+    {
+        /// <summary>
+        /// Time to wait before returning to pool.
+        /// </summary>
+        int WaitTime { get; }
+        
+        /// <summary>
+        /// Is the object initialized?
+        /// </summary>
+        bool IsInitialized { get; }
+
+        /// <summary>
+        /// Called by the pool when the object is first created to perform any necessary initialization.
+        /// </summary>
+        void Initialize(params object[] args);
+
+        /// <summary>
+        /// Called by pool when object instantiated.
+        /// </summary>
+        void OnCreatedInPool();
+
+        /// <summary>
+        /// Called by pool before you get a pool's object. Useful for state resetting.
+        /// </summary>
+        void OnGettingFromPool();
+        
+        /// <summary>
+        /// Called by pool when you return a pool's object. Useful for state resetting.
+        /// </summary>
+        void OnReturnToPool();
+        
+        /// <summary>
+        /// Return the object back to the pool after some time
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator ReturnToPoolAfterTime();
+    }
+}
